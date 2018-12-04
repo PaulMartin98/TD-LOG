9377418ab3bcc5cbdb8ec5482f08eb49afd96dbb
liamg <liamg@student.ethz.ch>
2018-12-04 12:32:35 +0100

    modif serv_test pour un super chat

--- serv_test/server.py
+++ serv_test/server.py
@@ -1,7 +1,8 @@
 
 
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, emit
+import random
 
 app = Flask(__name__)
 
@@ -15,7 +16,22 @@ def index():
 
 @socketio.on('message')
 def handle_message(msg):
+	print(msg)
 	send(msg, broadcast=True)
+
+@socketio.on('pingg')
+def handle_pingg(json):
+	print('ping')
+	# send('user ' + json["user"] + 'pinged for the '
+	# + json["nb_ping"] + " time", broadcast=True)
+	send('user '+str(json["id"]) + ' : pong ' + str(json["vping"]),
+	broadcast = True)
+
+@socketio.on('authentification')
+def handler_auth():
+	id = random.randint(0,10)
+	print(id)
+	emit('authentification',id)
 
 
 users = []
