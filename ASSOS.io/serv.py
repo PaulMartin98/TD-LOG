ec06b24b8b4ab9cbca10be48e8d41af314986843
liamg <liamg@student.ethz.ch>
2018-12-10 15:32:10 +0100

    changes

--- ASSOS.io/serv.py
+++ ASSOS.io/serv.py
@@ -11,35 +11,33 @@ width, height = 1000,1000
 dt = 0.01
 r = 5
 socketio = SocketIO(app)
-users = {}
+
+players = {}
 bullets = []
 map_state = [[-1]*width]*height
 
-
-
-def update_map():
-	# for i in range(bullets):
-	# 	bullets[i] = bullets[i].move_bullet()
-	for user, data in users:
-		users[user]["pos"] = data["pos"]+data["speed"]*dt
-	socketio.emit("update",users,broadcast = True)
-
 @app.route('/')
 def index():
-	return render_template('client.html')
+	return render_template('client_game.html')
 
 @socketio.on('new_connection')
 def handle_new_connection():
 	print("new player connected")
 	id = random.randint(0,100)
-	users[id] = {"pos" : [0,0],"speed" : [0,0] }
+	print(id)
+	users[id] = {"x" :0, "y" : 0, "vx" : 0,"vy" : 0 }
 	emit('authentification',id,0,0)
-	emit('users_update',users)
 
 @socketio.on('client_speed_update')
-def handle_move(user_id, speedx,speedy):
+def handle_move(user_id, vx, vy):
 	users[user_id]['speed'] = speed
+
+@socketio.on('client_request')
+def handle_client_request():
+  socket.emit('server_update',players, broadcast = True)
+
 
 
 if __name__ == '__main__':
+	# app.debug = True
 	socketio.run(app, host='localhost', port=5000)
